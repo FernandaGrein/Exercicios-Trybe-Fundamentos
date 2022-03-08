725de3820dff0d3f252fec0c89855cd9c4a24de8
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-03-08 12:04:57 -0300

    realiza exercícios 1, 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercício41.js
@@ -0,0 +1,24 @@
+const a = 15;
+const b = 3
+console.log (a +b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
+console.log (Math.max (a,b));
+const c = 10;
+console.log (Math.max (a,b,c));
+
+if ( a>b) {
+    console.log (a)
+} else {
+    console.log (b)
+};
+
+if (a>b && a>c) {
+    console.log (a)
+} else if (b>a && b>c) {
+    console.log (b)
+} else {
+    console.log (c)
+};
