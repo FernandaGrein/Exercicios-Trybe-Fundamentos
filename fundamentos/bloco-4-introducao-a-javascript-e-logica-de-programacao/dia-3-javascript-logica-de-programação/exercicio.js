c396c8e81632b7e188d4bd7ec77051e0f2b341d9
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-03-09 19:24:45 -0300

    resolve o exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programação/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programação/exercicio.js
@@ -13,5 +13,37 @@ for (let index =1; index< factorial.length; index+=1) {
         result += factorial[index] * result
     } 
     
-    
 } console.log(result)
+
+
+//desenvolva um algoritmo que é capaz de inverter uma palavra
+//let word = 'tryber';
+
+//criar a variavel com uma string
+//criar uma variável para armazenar o resultado
+//criar um looping que percorra a string 
+
+
+let word = 'tryber';
+let inversion ='';
+for (let index2 = word.length-1; index2 >=0; index2 -=1) {
+    inversion += word[index2]; 
+    }
+ 
+ console.log('resultado' , inversion); 
+
+
+
+//3
+//Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. 
+//Considere o número de caracteres de cada palavra.
+//let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// abrir uma variável que se inicia na primeira posição do array para adicionar a maior palavra
+//percorrer o array verificando o tamanho de cada palavra
+//se a palavra da posição atual for menor que a palavra da posição analisada
+// a varíavel é alterada para a palavra da posição analisada 
+// o loop corre até que todas as palavras sejam comparadas
+// imprime no terminar a maior palavra encontrada
+// o processo é o mesmo para localizar a menor palavra
+
