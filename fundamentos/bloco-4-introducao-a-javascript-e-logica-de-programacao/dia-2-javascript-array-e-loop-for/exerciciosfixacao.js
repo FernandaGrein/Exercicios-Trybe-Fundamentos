9f1a5796844049850a8b3cc8e7d0d164953aa964
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-03-09 11:28:39 -0300

    adiciona exercícios 4 a 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exerciciosfixacao.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exerciciosfixacao.js
@@ -20,3 +20,30 @@ for (var indice =0; indice < numbers.length; indice+=1) {
 console.log(media);
 
 //4
+let valor = 20
+for (var indice =0; indice < numbers.length; indice+=1) {
+    media = soma/ numbers.length
+} 
+console.log(media);
+if (media > valor) {
+    console.log("valor maior que 20")
+} else { 
+    console.log ("valor menor ou igual a 20")
+}; 
+
+//5 
+let maiorValor = numbers[0]
+for (var indice =1; indice < numbers.length; indice+=1){ 
+ if (numbers [indice] > maiorValor) {
+    maiorValor = numbers[indice];
+ }
+}
+console.log (maiorValor)
+
+//6
+let numerosImpares = 0;
+for (var indice =0; indice < numbers.length; indice+=1)
+if ( numbers[indice] % 2 !== 0) {
+    numerosImpares +=1
+} console.log(numerosImpares);
+
