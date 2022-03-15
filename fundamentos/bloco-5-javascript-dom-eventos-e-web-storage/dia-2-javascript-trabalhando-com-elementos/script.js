13b5bf1ccfb9623d002494b8f12c09c8f9822360
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-03-15 16:02:50 -0300

    realiza o terceiro exercício de fixação

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -32,3 +32,36 @@ const cadeOAtencao = document.getElementById("elementoOndeVoceEsta").parentNode.
 
 const filho3DoPai = document.getElementById("pai").childNodes[5]
 //console.log(filho3DoPai)
+
+//Crie um irmão para elementoOndeVoceEsta .
+//Crie um filho para elementoOndeVoceEsta .
+//Crie um filho para primeiroFilhoDoFilho .
+//A partir desse filho criado, acesse terceiroFilho ***.
+
+document.getElementById("elementoOndeVoceEsta")
+let texto = "texto adicionado para o elemento irmão criado"
+let ondeVamosCriar = document.getElementById("pai");
+let elementoIrmao = document.createElement("p"); 
+elementoIrmao.innerText = texto
+ondeVamosCriar.appendChild(elementoIrmao)
+
+document.getElementById("elementoOndeVoceEsta")
+let frase = "texto adicionado ao filho de onde eu estou"
+let novoLugar = document.getElementById("elementoOndeVoceEsta");
+let filhoDeOndeEstou = document.createElement("h1")
+filhoDeOndeEstou.innerText = frase
+novoLugar.appendChild(filhoDeOndeEstou)
+
+let desenho = "*-* *-* *-* ";
+let terceiroLugar = document.getElementById("primeiroFilhoDoFilho");
+let filhoDoFilho = document.createElement ("h2");
+filhoDoFilho.innerText = desenho;
+terceiroLugar.appendChild(filhoDoFilho)
+
+//Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta 
+// e primeiroFilhoDoFilho .
+//removi o primeiro e o terceiro filho
+
+pai.removeChild(filhoApartirdoPai)
+pai.removeChild (terceiroFilho)
+
