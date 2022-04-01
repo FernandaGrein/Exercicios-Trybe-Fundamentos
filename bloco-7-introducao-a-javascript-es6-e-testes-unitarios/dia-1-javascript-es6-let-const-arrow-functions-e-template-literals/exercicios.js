1b08122ce843a6bffcfc648dded5749f38e7ae75
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-04-01 18:32:34 -0300

    adiciona arquivo para os exercícios

--- bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios.js
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios.js
@@ -26,4 +26,4 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉 
